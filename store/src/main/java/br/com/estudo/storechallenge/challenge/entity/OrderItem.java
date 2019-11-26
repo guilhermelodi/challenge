7c2b82e1172c40f130e75c363ea52afa222e8248
Guilherme Lodi <guilherme.tlodi@gmail.com>
2019-11-26 15:01:06 -0300

    Creating specific project for Store

---
+++ store/src/main/java/br/com/estudo/storechallenge/challenge/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package br.com.estudo.storechallenge.challenge.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    private Long id;
+
+    private String description;
+
+    @Column(name = "unit_price")
+    private Double unitPrice;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "update_date")
+    private Date updateDate;
+
+}
---
+++ store/src/main/java/br/com/estudo/storechallenge/challenge/entity/Store.java
@@ -0,0 +1,34 @@
+package br.com.estudo.storechallenge.challenge.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@Table(name = "store")
+public class Store {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @OneToMany(mappedBy = "store")
+    private List<Order> orders;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "update_date")
+    private Date updateDate;
+
+}
